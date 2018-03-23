1686658d49d37711713944a40c1a325ff98cc39b
bdjyw_github <lyc_jie@163.com>
2018-03-23 14:26:41 +0800

    全部提交

---
+++ app/src/main/java/com/idophin/githubtest/HomeActivity.java
@@ -0,0 +1,7 @@
+package com.idophin.githubtest;
+
+import android.app.Activity;
+
+public class HomeActivity extends Activity {
+
+}
---
+++ app/src/main/java/com/idophin/githubtest/User.java
@@ -0,0 +1,10 @@
+package com.idophin.githubtest;
+
+/**
+ * 用户
+ * Created by liyanchuan on 2018/3/23.
+ */
+
+public class User {
+    private String name;
+}
