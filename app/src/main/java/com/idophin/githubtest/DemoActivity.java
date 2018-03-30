2c585734ffdac74e9bfe788af0094c23a9105122
bdjyw_github <lyc_jie@163.com>
2018-03-30 10:28:41 +0800

    add tag101

---
+++ app/src/main/java/com/idophin/githubtest/DemoActivity.java
@@ -0,0 +1,9 @@
+package com.idophin.githubtest;
+import android.app.Activity;
+
+public class DemoActivity extends Activity {
+
+    private String aaa;
+    //add tag101
+    private String bbb;
+}
